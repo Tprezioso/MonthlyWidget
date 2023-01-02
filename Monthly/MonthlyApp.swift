77be07f2a2b6e18f16f5e1fd5f2e2138f653de9c
Thomas <tommyprezioso@gmail.com>
2023-01-02 12:26:27 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Thomas Prezioso Jr on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
