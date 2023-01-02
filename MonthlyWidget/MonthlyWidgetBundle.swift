e1df970272c5756ac147841a0e48ff38203d4ad3
Thomas <tommyprezioso@gmail.com>
2023-01-02 14:10:45 -0500

    first commit

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Thomas Prezioso Jr on 1/2/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
